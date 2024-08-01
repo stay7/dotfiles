eea1783d309db637393436ae9fb3ae1a302bdba1
stay7 <codelatte20@gmail.com>
2024-08-02 00:47:14 +0900

    change colorscheme ayu-theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,10 @@
 return {
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-latte",
-    },
-  },
-  -- colorscheme
-  {
     "ayu-theme/ayu-vim",
     lazy = false,
     priority = 1000,
     config = function()
       vim.cmd("colorscheme ayu")
-      vim.cmd("highlight clear SignColumn")
     end,
   },
 }
