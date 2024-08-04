19b27ab8af3daa2e820b9a344beeedda1723d8e1
stay7 <codelatte20@gmail.com>
2024-08-04 23:06:34 +0900

    Update lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,8 @@ local map = LazyVim.safe_keymap_set
 
 map("n", "<C-i>", "<C-o>", { desc = "swap jumplist key", remap = true })
 map("n", "<C-o>", "<C-i>", { desc = "swap jumplist key", remap = true })
+
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("x", "<leader>p", [["_dP]])
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,15 @@
+function SetBackground()
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
   {
     "ayu-theme/ayu-vim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme ayu")
+      SetBackground()
     end,
   },
 }
