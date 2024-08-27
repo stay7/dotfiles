cf6fa79b841aabb775726dc79c5a34ef1fbd2af3
stay7 <codelatte20@gmail.com>
2024-08-28 01:00:32 +0900

    Set open current buffer

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,13 +3,14 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			html = { "prettier" },
-			css = { "prettier" },
-			javascript = { "prettier" },
-			typescriptreact = { "prettier" },
-			markdown = { "prettier" },
+			json = { "prettier", lsp_fallback = true },
+			yaml = { "prettier", lsp_fallback = true },
+			html = { "prettier", lsp_fallback = true },
+			css = { "prettier", lsp_fallback = true },
+			javascript = { "prettier", lsp_fallback = true },
+			typescript = { "prettier", lsp_fallback = true },
+			typescriptreact = { "prettier", lsp_fallback = true },
+			markdown = { "prettier", lsp_fallback = true },
 		},
 		format_on_save = {
 			timeout_ms = 500,
--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -8,4 +8,7 @@ return {
 	config = function()
 		require("nvim-tree").setup({})
 	end,
+	keys = {
+		{ "<leader>E", "<cmd>NvimTreeFindFile<cr>", desc = "open current Buffer" },
+	},
 }
